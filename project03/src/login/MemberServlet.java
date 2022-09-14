bbd46c177cab47878523093d917a0ce454bf1d0b
vnjl <lsheayenugk1@naver.com>
2022-09-14 18:12:11 +0900

    JSP 0914 1812

---
+++ project03/src/login/MemberServlet.java
@@ -0,0 +1,75 @@
+package login;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Date;
+import java.util.List;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import login.MemberDAO;
+import login.MemberVO;
+
+@WebServlet("/member4")
+public class MemberServlet extends HttpServlet{
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doHandle(request, response);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doHandle(request, response);
+	}
+	
+	private void doHandle(HttpServletRequest request,HttpServletResponse response)  throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html;charset=utf-8");
+		//SQL문으로 조회할 MemberDAO 객체를 생성
+		MemberDAO dao=new MemberDAO();
+		PrintWriter out=response.getWriter();
+
+		String command=request.getParameter("command");
+	      
+		if(command!= null && command.equals("addMember")){
+			 String _id=request.getParameter("id");
+			 String _pwd=request.getParameter("pwd");
+			 String _name=request.getParameter("name");
+			 String _email=request.getParameter("email");
+			 
+			 MemberVO vo=new MemberVO();
+			 vo.setId(_id);
+			 vo.setPwd(_pwd);
+			 vo.setName(_name);
+			 vo.setEmail(_email);	
+			 dao.addMember(vo);
+		}
+		List list=dao.listMembers();
+		out.print("<html><body>");
+		out.print("<table border=1><tr align='center' bgcolor='lightgreen'>");
+		out.print("<td>아이디</td><td>비밀번호</td><td>이름</td><td>이메일</td><td>가입일</td><td >삭제</td></tr>");
+		for (int i=0; i<list.size(); i++){
+			MemberVO memberVO=(MemberVO) list.get(i);
+	 		String id=memberVO.getId();
+	 		String pwd = memberVO.getPwd();
+	 		String name = memberVO.getName();
+	 		String email =memberVO.getEmail();
+	 		Date joinDate = memberVO.getJoinDate();	
+	 		out.print("<tr><td>"+id+"</td><td>"
+		                +pwd+"</td><td>"
+		                +name+"</td><td>"
+		                +email+"</td><td>"
+		                +joinDate+"</td><td>"
+	                    +"&nbsp;</td></tr>");	 		
+		}
+		out.print("</table></body></html>");
+		out.print("<a href='/project03/sub01/sub03/sub03.html'>새 회원 등록하기</a>");
+	}
+}
